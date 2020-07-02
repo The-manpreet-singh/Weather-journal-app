e467bad4e1ae264552fb0375ee57cd44b280edfa
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-07-02 08:11:40 +0530

    add key

---
+++ website/main.js
@@ -0,0 +1,3 @@
+// Personal API Key for OpenWeatherMap API
+const API_KEY = '&APPID=e23122c5062eb361eb2aa6ee3762e1db&units=imperial';
+const baseURL = 'https://api.openweathermap.org/data/2.5/weather?q=';
